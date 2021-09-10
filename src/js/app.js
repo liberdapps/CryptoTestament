5fb9b1e3dcb28de79be5ae813a4da9ef1c849c84
liberdapps <90274757+liberdapps@users.noreply.github.com>
2021-09-10 01:42:42 +0000

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -24,31 +24,44 @@ initContract: function () {
       // Connect provider to interact with contract
       App.contracts.CryptoWill.setProvider(App.web3Provider);
 
-
-    Vue.component('main-page', {
-      template: '#main-page-template',
-      data () {
-        return {
-          lang: 'pt',
-          langs: {
-            'en': {
-              title: 'Crypto Will',
-              subtitle: 'Your keys, your coins, your will!',
-              launchDApp: 'Launch DApp',
-              intro: 'Bla bla',
-            },
-            'pt': {
-              title: 'Crypto Will',
-              subtitle: 'Your keys, your coins, your will!',
-              launchDApp: 'Acessar DApp',
-              intro1: 'Com as criptomoedas, você assume o controle sobre o seu próprio dinheiro.',
-              intro2: 'Com o Crypto Will, você define quem fica com ele na sua ausência.',
-            }
+      let langsObj = {
+        lang: 'pt',
+        langs: {
+          'en': {
+            title: 'Crypto Will',
+            subtitle: 'Your keys, your coins, your will!',
+            launchDApp: 'Launch DApp',
+            intro: 'Bla bla',
+            broughtBy: 'Brought by '
+          },
+          'pt': {
+            title: 'Crypto Will',
+            subtitle: 'Your keys, your coins, your will!',
+            launchDApp: 'Acessar DApp',
+            intro1: 'Com as criptomoedas, você assume o controle sobre o seu próprio dinheiro.',
+            intro2: 'Com o Crypto Will, você define quem fica com ele na sua ausência.',
+            broughtBy: 'Um produto da '
           }
         }
-      }
-    });
+      };
 
+      Vue.component('main-page', {
+        template: '#main-template',
+        data () {
+          return langsObj;
+        }
+      });
+
+      Vue.component('dapp-page', {
+        template: '#dapp-template',
+        data () {
+          return langsObj;
+        },
+        mounted() {
+
+        }
+      });
+  
     vueApp = new Vue({
       el: '#app'
     })
@@ -90,5 +103,28 @@ connectWallet() {
 $(function () {
   $(window).load(function () {
     App.init();
+
+    var tabs = $('.tabs');
+    var selector = $('.tabs').find('a').length;
+    //var selector = $(".tabs").find(".selector");
+    var activeItem = tabs.find('.active');
+    var activeWidth = activeItem.innerWidth();
+    $(".selector").css({
+      "left": activeItem.position.left + "px", 
+      "width": activeWidth + "px"
+    });
+
+    $(".tabs").on("click","a",function(e){
+      e.preventDefault();
+      $('.tabs a').removeClass("active");
+      $(this).addClass('active');
+      var activeWidth = $(this).innerWidth();
+      var itemPos = $(this).position();
+      $(".selector").css({
+        "left":itemPos.left + "px", 
+        "width": activeWidth + "px"
+      });
+    });
+
   });
 });
